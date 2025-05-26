9a780231bf9209707b7be91507c927c3cb6cee1e
Yaya sanyang <yasanyang2000@gmail.com>
2025-05-26 23:56:33 +0000

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+const App = () => {
+  return <h1>Hello world!</h1>;
+};
+
+export default App;
